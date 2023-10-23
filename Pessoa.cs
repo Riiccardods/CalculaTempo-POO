df74a79ebc148d659282a877e45e027bc72c3915
riicardods <riicardoljn@icloud.com>
2023-10-23 20:50:17 -0300

    Adicionar arquivos de projeto.

---
+++ Pessoa.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace pooCalculaIdade
+{
+    internal class Pessoa
+    {
+
+
+		private int anoNascimento;
+
+		public int AnoNascimento
+		{
+			get { return this.anoNascimento; }
+			set { this.anoNascimento = value; }
+		}
+
+
+		private string nome;
+
+		public string Nome
+		{
+			get { return nome; }
+			set { nome = value.ToUpper(); }
+		}
+
+
+		public void ExibirDados() 
+		{
+			Console.WriteLine("Nome: " + this.nome);
+            Console.WriteLine("Ano de nascimento: " + this.AnoNascimento);
+			int idade = this.CalcularIdade();
+            Console.WriteLine("Idade: " + idade);
+		}
+
+		private int CalcularIdade() 
+		{
+			DateTime data = DateTime.Now;
+			int ano = data.Year;
+			int idade = ano - this.anoNascimento;
+			return idade;
+		}
+
+
+
+	}
+}
---
+++ Program.cs
@@ -0,0 +1,26 @@
+﻿namespace pooCalculaIdade
+{
+    internal class Program
+    {
+
+
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Programa que calcula a idade de uma Pessoa");
+            Pessoa p = new Pessoa();
+            Console.WriteLine("nome da pessoa");
+            p.Nome = Console.ReadLine();
+
+            Console.WriteLine("Ano de nascimento");
+            p.AnoNascimento = Convert.ToInt32(Console.ReadLine());
+
+            p.ExibirDados();
+
+
+        }
+
+
+
+    }
+}
